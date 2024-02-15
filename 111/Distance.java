25a4f98d5e26a40a4c825948b65316d6a5544b2a
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-02-15 17:28:20 -0600

    Create Distance.java

---
+++ 111/Distance.java
@@ -0,0 +1,26 @@
+/* 
+Reference:
+Math.pow(a, b) ; = a^b
+*/
+
+import java.util.Scanner ;
+
+public class Distance {
+	public static void main (String [] args) {
+	double x1 , x2 , y1 , y2 , dist ; 			                                   // Declare variables
+	
+	Scanner input = new Scanner(System.in) ; 
+	System.out.print("Enter x1: ");
+	x1 = input.nextDouble() ; 
+	System.out.print("Enter x2: ");
+	x2 = input.nextDouble() ;
+	System.out.print("Enter y1: ");
+	y1 = input.nextDouble() ;
+	System.out.print("Enter y2: ");
+	y2 = input.nextDouble() ;			                                              // Ask for inputs for variables
+	
+	dist = Math.sqrt((Math.pow((x2 - x1),2.0) + Math.pow((y2 - y1),2))) ;       // Calculate distance
+	
+	System.out.printf("The distance between the two points is %.1f" , dist) ;   // Display the result with 1 decimal place
+	}
+}
