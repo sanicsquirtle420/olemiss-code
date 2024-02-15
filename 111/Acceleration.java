a6f3469c98c98a94774163c7e0b7aba22ef4b6b2
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-02-15 17:37:16 -0600

    Create Acceleration.java

---
+++ 111/Acceleration.java
@@ -0,0 +1,20 @@
+import java.util.Scanner ; 
+
+public class Acceleration {
+	public static void main (String [] args) {
+		double v0, v1 , t , acc ;
+		
+		Scanner input = new Scanner(System.in) ; 
+		System.out.print("Enter starting velocity: ") ;
+		v0 = input.nextDouble() ; 
+		System.out.print("Enter ending velocity: ") ;
+		v1 = input.nextDouble() ;
+		System.out.print("Enter the time: ") ;
+		t = input.nextDouble() ;
+		
+		acc = (v1 - v0) / t ;
+		
+		System.out.printf("The acceleration is %.2f" , acc) ;
+		
+	}
+}
