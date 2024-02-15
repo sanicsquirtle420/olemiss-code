35afdcfb502f5c925bd3d9a75e0c03366e21ce76
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-02-15 17:21:32 -0600

    Create ComputeArea.java

---
+++ 111/ComputeArea.java
@@ -0,0 +1,18 @@
+import java.util.Scanner ;                             // In Scanner We Trust
+
+public class ComputeArea {
+	public static void main(String [] args) {
+		double raidus ;
+		double area	;	                                     // Declare variables
+		final double PI = 3.1415 ;
+		
+		Scanner input = new Scanner(System.in) ; 
+		System.out.print("Enter the radius: ") ; 
+		raidus = input.nextDouble() ;				               // Ask the user for radius
+		
+  	area = PI * Math.pow(radius, 2) ;                  // Calculate the area
+  		
+		System.out.println("The area is " + area);         // Display the area
+		System.out.printf("The area is %.2f" , area) ;
+	}
+}
