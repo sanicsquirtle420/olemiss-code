44459883d5b6acea19da58b23a6c4ca1e601860e
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-02-15 17:35:45 -0600

    Create Temperature.java

---
+++ 111/Temperature.java
@@ -0,0 +1,19 @@
+/*
+Notes:
+'(int)fah' is needed to force fah to convert to an integer.
+*/
+import java.util.Scanner ;
+
+public class Temperature {
+	public static void main (String [] args) {
+		double cel , fah ;
+		
+		Scanner input = new Scanner(System.in) ; 
+		System.out.print("Enter the celsius degree: ") ;
+		cel = input.nextDouble() ; 
+		
+		fah = ( (9/5.0) * cel + 32) ;
+		
+		System.out.printf("The fahrenheit degree is %d" , (int)fah) ;
+	}
+}
