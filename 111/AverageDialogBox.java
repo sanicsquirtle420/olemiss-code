2e4ddc4384ab62344035a6682cbabe253f122ea5
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-02-15 17:33:10 -0600

    Create AverageDialogBox.java

---
+++ 111/AverageDialogBox.java
@@ -0,0 +1,25 @@
+/*
+Notes:
+This is the same program as olemiss-java/111/Average.java 
+except it is using JOptionPane's dialog boxes. 
+*/
+
+import javax.swing.JOptionPane ;                                       // Import statement
+
+public class AverageDialogBox {
+	public static void main (String [] args) {
+		String num1str , num2str ;
+		int num1 , num2;
+		double average ; 													                         // Declare variables
+		
+		num1str = JOptionPane.showInputDialog("Enter first integer. ") ;
+		num1 = Integer.parseInt(num1str) ; 
+		num2str = JOptionPane.showInputDialog("Enter second integer. ") ;   // Asking the user for integers
+		num2 = Integer.parseInt(num2str) ; 									                // Converting string to integer
+		
+		average = ((num1 + num2) / 2.0);									                  // Calculate average
+		
+		JOptionPane.showMessageDialog(null, "The average is " + average); 	// Display
+		System.exit(0) ;
+	}
+}
