685299176f1e5154486ddaf151dcf785b1dad7d4
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-02-15 17:39:04 -0600

    Create ShoeConversion.java

---
+++ 111/ShoeConversion.java
@@ -0,0 +1,16 @@
+import java.util.Scanner ;
+
+public class ShoeConversion {
+	public static void main (String [] args) {
+		double foot, euro ;
+		
+		Scanner input = new Scanner (System.in) ;
+		System.out.print("Enter your foot size in inches ");
+		foot = input.nextDouble() ;
+		foot = foot * 2.54 ;
+		
+		euro = (3/2.0) * (foot + 1.5) ;
+		
+		System.out.printf("Your European shoe size is %d" , (int)euro) ; 
+	}
+}
