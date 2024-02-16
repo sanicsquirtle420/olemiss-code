42ab9d19d97883c8903517d91742058d2cb7b85c
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-02-16 14:33:31 -0600

    Update Driving.java

--- 111/Driving.java
+++ 111/Driving.java
@@ -1,8 +1,8 @@
 import java.util.Scanner ; 
 
 public class Driving {
-	public static void main (String [] args) { 							      // import statement(s), class header, and main method header
-		double dist , mi , gal , cost ; 								            // declare variables for the inputs and output
+	public static void main (String [] args) {	// import statement(s), class header, and main method header
+		double dist , mi , gal , cost ;		// declare variables for the inputs and output
 		
 		Scanner input = new Scanner(System.in) ;
 		System.out.print("Enter the driving distance: ") ;
@@ -10,9 +10,9 @@ public class Driving {
 		System.out.print("Enter miles per gallon: ") ;
 		mi = input.nextDouble () ;
 		System.out.print("Enter price per gallon: ") ;
-		gal = input.nextDouble () ; 									                // prompt for inputs
+		gal = input.nextDouble () ;	 // prompt for inputs
 		
-		cost = (dist / mi) * gal ; 										                // calculate cost of driving
+		cost = (dist / mi) * gal ;	 // calculate cost of driving
 		
 		System.out.printf("\nThe cost of driving is $%.2f" , cost) ; 	// display the output (2 decimal places)
 	}
