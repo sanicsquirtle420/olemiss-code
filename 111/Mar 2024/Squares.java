1c2a214e6a0195a32cf70468e5542286c0d89e3b
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-03-27 14:51:35 -0500

    Update Squares.java
    SquaresV2.java

--- 111/Mar 2024/Squares.java
+++ 111/Mar 2024/Squares.java
@@ -1,13 +1,24 @@
-// 03/25/24
+/* 
+UPDATED: 03/27/24
+Now the program gives the squares of numbers from the minInt to 
+the maxInt. (I like this program more than the old one so it got
+updated)
+*/
+import java.util.Scanner ;
+
 public class Squares {
 	public static void main(String [] args) {
-		int num = 1 , total = 0; 
+		int minInt , maxInt; 
 		
-		while(num <= 10) {
-			System.out.printf("%d\t%d\n" , num , num*num) ; // \t is a tab
-			total += num*num ; 
-			num++ ;
+		Scanner input = new Scanner(System.in) ; 
+		
+		System.out.print("Enter the minimum integer: ") ;
+		minInt = input.nextInt() ;
+		System.out.print("Enter the maximum integer: ") ; 
+		maxInt = input.nextInt() ;
+		
+		for (int i = minInt ; i <= maxInt ; i++) {
+			System.out.printf("%d\t%d\n" , i , i*i) ;
 		}
-		System.out.printf("\nThe sum of all squares is %d" , total) ;
 	}
 }
