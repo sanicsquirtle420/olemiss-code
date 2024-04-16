288896f6cc3bafd1f4d81bbaf605fcf3a977b13b
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-04-16 12:20:50 -0500

    Create ArrayEx.java

---
+++ 111/Apr 2024/ArrayEx.java
@@ -0,0 +1,42 @@
+import java.util.Random ; // 04/16/24
+
+public class ArrayEx {
+	public static void main(String [] args) {
+		int[] intArray = new int[5] ;
+		
+		Random rand = new Random() ;
+		
+		for (int i = 0 ; i < intArray.length ; i++) { // giving random values to array
+			intArray[i] = rand.nextInt(21) ; // 0 - 20
+		}
+		
+		System.out.print("The array contains: ") ; // printing the array
+		for (int value : intArray) {
+			System.out.print(value + " ") ;
+		}
+		
+		int sum = 0 ;
+		for (int i = 0 ; i < intArray.length ; i++) // the sum of the array
+			sum += intArray[i] ;
+		
+		System.out.printf("\nThe sum is %d." , sum) ;
+		
+		int max = intArray[0] , min = intArray[0] ;
+		int maxIndex = 0 , minIndex = 0 ;
+		
+		for (int i = 0 ; i < intArray.length ; i++) {
+			if (intArray[i] > max) { // getting the max and maxIndex of array
+				max = intArray[i] ;
+				maxIndex = i ;
+			}
+			
+			if (intArray[i] < min) { // getting the min and minIndex of array
+				min = intArray[i] ;
+				minIndex = i ;
+			}
+		}
+		
+		System.out.printf("\nThe max is %d and the index is %d" , max , maxIndex) ;
+		System.out.printf("\nThe minimum is %d and the index is %d" , min, minIndex) ;
+	}
+}
