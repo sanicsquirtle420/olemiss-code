51cf3c3cc4fdc875c25116ae4644baa7f9c49b67
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-10-16 19:24:50 -0500

    Update SortBubble.java
    finally added the code for the bubble sort :laughing:

--- 112/Notes/edu/olemiss/week05/SortBubble.java
+++ 112/Notes/edu/olemiss/week05/SortBubble.java
@@ -1,8 +1,36 @@
 package edu.olemiss.week05 ;
 
 public class SortBubble {
-    public static void main(String[] args) {
-        /** Bubble Sorting */
+    private int[] numbers = {72, 4, 37, 83, 9, 43, 17, 28, 7, 1, 55, 61} ;
 
+    public void bubbleSort() {
+        int temp = 0 ; // temp value for the swap
+
+        for(int out = 0 ; out < numbers.length ; out++) {
+            // starts at the first index
+            for(int in = 1 ; in < (numbers.length - out) ; in++) {
+                // starts at the second index
+                if(numbers[in - 1] > numbers[in]) {
+                    // tests the value of the previous cell to the current cell
+                    temp = numbers[in - 1] ; // previous value to temp
+                    numbers[in - 1] = numbers[in] ; // current value to the previous cell
+                    numbers[in] = temp ; // temp value to the current cell
+                }
+            }
+        }
+    }
+
+    public void printArray() {
+        for(int i = 0 ; i < numbers.length ; i++) {
+            System.out.printf("%d: %d\n" , i , numbers[i]) ;
+        }
+    }
+
+    public static void main(String[] args) {
+        SortBubble test = new SortBubble() ;
+
+        test.bubbleSort() ; // does the sort
+        test.printArray() ; // prints the sort
     }
 }
+
