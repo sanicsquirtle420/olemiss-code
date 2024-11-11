14345fcc4015246a25644d62a5dff86c4a1f558b
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-11-11 11:26:14 -0600

    Create mergeSort.java
    - merge sort with recursion

--- 112/Notes/edu/olemiss/week11/mergeSort.java
+++ 112/Notes/edu/olemiss/week11/mergeSort.java
@@ -1,41 +1,7 @@
+package edu.olemiss.week11 ;
 
-public class Sorts {
-	private int[] array = new int[0];
-
-	public void quickSort(int min, int max){
-		int pivot;
-		if(min < max){
-			pivot = partition(min, max);
-			quickSort(min, (pivot-1));
-			quickSort((pivot + 1), max);
-		}
-	}
-	
-	public int partition(int min, int max){
-		int pivot = array[min];
-		int left = min;
-		int right = max;
-		while(left < right){
-			while(array[left] <= pivot && left < right){
-				left++;
-			}
-			while(array[right] > pivot){
-				right--;
-			}
-			if(left < right){
-				swap(left, right);
-			}
-		}
-		swap(min, right);
-		
-		return right;
-	}
-	
-	public void swap(int left, int right){
-		int temp = array[left];
-		array[left] = array[right];
-		array[right] = temp;
-	}
+public class mergeSort {
+	private int[] array = new int[20];
 	
 	public void mergeSort(int min, int max){
 		if(min < max){
