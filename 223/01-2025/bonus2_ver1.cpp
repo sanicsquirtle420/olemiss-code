ddb16fc1ad696f2b02857ae4bd3b1c0815b846bd
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2025-02-03 15:37:25 -0600

    Update bonus2_ver1.cpp (HW 2, bonus)
    - no more segmentation fault :D 
    Fix: allocated memory for the larger 2D arrays

--- 223/01-2025/bonus2_ver1.cpp
+++ 223/01-2025/bonus2_ver1.cpp
@@ -8,10 +8,14 @@ int main(void) {
 	struct timeval beg , end ;
 	gettimeofday(&beg , 0) ;
 
+    // allocating space for larger arrays
+    int** src = new int* [2048] ;
+    int** dst = new int* [2048] ;
 
-  // TODO: allocate memory for these large arrays
-	int src[2048][2048] ; 
-	int dst[2048][2048] ;
+    for(int i = 0 ; i < 2048 ; i++) {
+        src[i] = new int[2048] ;
+        dst[i] = new int[2048] ;
+    }
 
 	// version 1
 	for(int i = 0 ; i < 2048 ; i++) {
@@ -25,6 +29,16 @@ int main(void) {
 	long microsec = end.tv_usec - beg.tv_usec ;
 	double total = sec + microsec * 1e-6 ;
 
+    // deallocating memory
+    for(int i = 0 ; i < 2048 ; i++) {
+        delete[] src[i] ;
+        delete[] dst[i] ;
+    }
+
+    delete[] src ;
+    delete[] dst ;
+
+    cout << "Bonus: Version 1" << endl ;
 	cout << "Time measured " << total << endl ;
 
 	return 0 ;
