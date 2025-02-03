e3774ffcb2a0058bed05fb1c9c4ae20ef0f4928c
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2025-02-03 15:41:19 -0600

    Create hw2_ver1.c (HW 2)

---
+++ 223/01-2025/hw2_ver1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <sys/time.h>
+
+int main(void) 
+{
+	// measuring time
+	struct timeval begin, end ;
+	gettimeofday(&begin , 0) ;
+
+	int src[512][512] ;
+	int dst [512][512] ;
+
+	// version 1
+	for(int i = 0 ; i < 512 ; i++) 
+	{
+		for(int j = 0 ; j < 512 ; j++) 
+		{
+			dst[j][i] = src[j][i] ;
+		}
+	}
+	
+	// ending time measuring
+	gettimeofday(&end , 0) ;
+	long sec = end.tv_sec - begin.tv_sec ;
+	long microsec = end.tv_usec - begin.tv_usec ;
+	double total = sec + microsec * 1e-6 ;
+
+	printf("Time measured: %.3f seconds.\n" , total) ;
+
+	return 0 ;
+}
