f59e4914a6fca55219802dd259ff78e8425953d6
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2025-02-18 23:44:02 -0600

    Update PreLab4.java

--- 211/PreLabs/04/PreLab4.java
+++ 211/PreLabs/04/PreLab4.java
@@ -10,20 +10,32 @@ public class PreLab4 {
 
     // TODO: make this method add the Marble before the first one it finds with the same color
     public void addByColor(Marble m, String color) throws InvalidMarbleException {
-        boolean found = false ;
-        for(Marble kiriko : marbleStack) {
-            if(kiriko.getColor().equals(color)) {
-                found = true ;
-                marbleStack.push(m) ;
-                break ;
+        Stack<Marble> tmp = new Stack<>() ;
+        boolean insert = false ;
+
+        while(!marbleStack.isEmpty()) {
+            Marble t = marbleStack.pop() ;
+            if(!insert && t.getColor().equals(color)) {
+                marbleStack.push(m);
+                insert = true;
             }
+            tmp.push(t) ;
         }
-        if(!found) {
+        while(!tmp.isEmpty()) {
+            marbleStack.push(tmp.pop()) ;
+        }
+        if(!insert) {
             throw new InvalidMarbleException("Invalid color") ;
         }
     }
 
     public void sortByColor() {
-        // write method
+        ArrayList<Marble> marList = new ArrayList<>(marbleStack) ;
+        marList.sort(Comparator.comparing(Marble :: getColor)) ;
+
+        marbleStack.clear() ;
+        for(int i = marList.size() - 1; i > 0 ; i--) {
+            marbleStack.push(marList.get(i)) ;
+        }
     }
 }
