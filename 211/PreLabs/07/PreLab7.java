84259b0753459fbeec4f20297f099821eb1d0fd0
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2025-03-18 00:02:25 -0500

    Update PreLab7.java
    - redid removeMales() properly 
    * used two nodes
    - fixed(?) NullPointerException, but isn't getting the sample result

--- 211/PreLabs/07/PreLab7.java
+++ 211/PreLabs/07/PreLab7.java
@@ -12,9 +12,9 @@ public class PreLab7 {
             String[] token = scan.nextLine().split(",") ;
             kiriko.addToRear(new Person(token[0] , Integer.parseInt(token[1]) , token[2])) ;
         }
-
-        removeMales(kiriko) ;
         System.out.println("Kiriko:\n" + kiriko) ;
+        removeMales(kiriko) ;
+        System.out.println("Kiriko (without Males):\n" + kiriko) ;
 
         add(kiriko , "samplePeople2.txt") ;
         System.out.println("Kiriko After:\n" + kiriko) ;
@@ -22,10 +22,22 @@ public class PreLab7 {
 
     public static void removeMales(LinkedUnorderedList<Person> lst) {
         LinearNode<Person> current = lst.getListHead() ;
+        LinearNode<Person> previous = null ;
 
         while(current != null) {
             if(Objects.equals(current.getElement().getGender(), "male")) {
-                lst.remove(current.getElement()) ;
+                if(previous == null) {
+                    // removes the head node
+                    lst.head = current.getNext() ;
+                } else {
+                    // skips the current node
+                    previous.setNext(current.getNext()) ;
+                }
+                lst.count-- ;
+                lst.modCount++ ;
+            } else {
+                // Move previous forward only if nothing is removed
+                previous = current ;
             }
 
             current = current.getNext() ;
@@ -33,23 +45,26 @@ public class PreLab7 {
     }
 
     public static void add(LinkedUnorderedList<Person> lst , String filename) throws FileNotFoundException {
-      // TODO: fix NullPointerException and this method
+        removeMales(lst) ;
+
         LinearNode<Person> current = lst.getListHead() ;
         LinearNode<Person> previous = null ;
 
         Scanner scan = new Scanner(new File(filename)) ;
 
-        while(current != null) {
-            // System.out.println("Juno") ;
-            while(scan.hasNextLine()) {
-                // System.out.println("Kiriko") ;
-                String[] token = scan.nextLine().split(",");
-                if (!Objects.equals(current.getElement().getName(), token[0])) {
-                    lst.addAfter(new Person(token[0], Integer.parseInt(token[1]), token[2]), previous.getElement());
+        while(scan.hasNextLine()) {
+            String[] token = scan.nextLine().split(",");
+            while(current != null) {
+                String cmp = current.getElement().getName() ;
+                if (!cmp.equalsIgnoreCase(token[0])) {
+                    lst.addAfter(new Person(token[0], Integer.parseInt(token[1]), token[2]), previous.getElement()) ;
+
+                } else {
+                    previous = current ;
+                    current = current.getNext() ;
                 }
+                break ;
             }
-            previous = current;
-            current = current.getNext();
         }
     }
 }
